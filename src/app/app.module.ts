6ad2e0ce7518369a3a989f8a6ceda7534f0bdd35
ojoubout <oussamajoubouti@gmail.com>
2022-01-06 11:51:09 +0100

    Login & Navbar design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { FortyTwoLogoComponent } from './shared/forty-two-logo/forty-two-logo.component';
+import { LeetLogoComponent } from './shared/leet-logo/leet-logo.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FortyTwoLogoComponent,
+    LeetLogoComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
