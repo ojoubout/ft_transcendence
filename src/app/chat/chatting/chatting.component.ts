3f8e0bba4c40d691429e2bb475a2b537d3c7f1b8
ojoubout <oussamajoubouti@gmail.com>
2022-01-25 13:06:48 +0100

    adding loading icon for messages

--- src/app/chat/chatting/chatting.component.ts
+++ src/app/chat/chatting/chatting.component.ts
@@ -1,7 +1,8 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from "@angular/core";
+import {Component, ElementRef, Input, OnInit, TemplateRef, ViewChild} from "@angular/core";
 import {ChatService} from "../chat.service";
 import {Chat} from "../shared/chat.model";
 import {Message} from "../shared/message.model";
+import {NgIfContext} from "@angular/common";
 
 @Component({
   selector: 'app-chatting',
@@ -11,12 +12,17 @@ import {Message} from "../shared/message.model";
 export class ChattingComponent {
 
   @ViewChild('content') content: ElementRef;
+  @ViewChild('messageTemplate') messageTemplate: TemplateRef<NgIfContext<boolean>>;
   readonly maxMsToShowDate: number = 60 * 60 * 1000; // 1 hour
+
+  loading = false;
 
   constructor(public chatService: ChatService) {
     if (this.chatService.currChat?.messages.length == 0) {
+      this.loading = true;
       this.chatService.loadMessages(this.chatService.currChat).subscribe({
         next: value => {
+          this.loading = false;
           this.scrollDown();
         }
       });
@@ -36,6 +42,10 @@ export class ChattingComponent {
 
   scrollDown() {
     this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
+  }
+
+  get chat() {
+    return this.chatService.currChat;
   }
   get messages() {
     if (!this.chatService.currChat) {
